e09c8aac38de5d0358c81cc2efdc8820b898997f
ndav <ndav.sb21@gmail.com>
2021-05-25 16:14:00 -0400

    Create 11 connect-four project

---
+++ app.js
@@ -0,0 +1,15 @@
+const WIDTH = 7;
+const HEIGHT = 6;
+
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+/** makeBoard: create in-JS board structure:
+ *   board = array of rows, each row is array of cells  (board[y][x])
+ */
+
+function makeBoard() {
+  for (let y = 0; y < HEIGHT; y++) {
+    board.push(Array.from({ length: WIDTH }));
+  }
+}
